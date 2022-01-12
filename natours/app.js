62d345dc03ba5226c5cd684d4093b3fcc9ffdf7b
Nguyễn Hoàng Anh <hoahoanganh20012001@gmail.com>
2022-01-12 20:09:49 +0700

    setup express

---
+++ natours/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+app.get('/', (req, res) =>{
+    res.status(200).send('Hello express !');
+})
+
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Sever runing on ${port}`);
+})
