6f0bc9a716212f3de46fa00cf801d0e8fd294ec8
Nguyễn Hoàng Anh <hoahoanganh20012001@gmail.com>
2022-07-09 01:51:44 +0700

    handle errors development and product

--- natours/middlewares/globalErrorHandler.js
+++ natours/middlewares/globalErrorHandler.js
@@ -1,13 +1,37 @@
+const sendErrorDev = (error, err, res) => {
+	res.status(error.statusCode).json({
+		status: error.status,
+		message: err.message,
+		error: err,
+		stack: err.stack,
+	});
+};
+
+const sendErrorProduct = (error, err, res) => {
+	if (err.isOperational) {
+		res.status(error.statusCode).json({
+			status: error.status,
+			message: err.message,
+		});
+	} else {
+		res.status(500).json({
+			status: 'error',
+			meassge: 'Something went very wrong !',
+		});
+	}
+};
+
 const globalErrorHandler = (err, req, res, next) => {
 	const error = {
 		statusCode: err.statusCode || 500,
 		status: err.status || 'error',
 	};
 
-	res.status(error.statusCode).json({
-		status: error.status,
-		message: err.message,
-	});
+	if (process.env.NODE_ENV === 'development') {
+		sendErrorDev(error, err, res);
+	} else if (process.env.NODE_ENV === 'product') {
+		sendErrorProduct(error, err, res);
+	}
 
 	next();
 };
