bd9d509756bca155a7f73241620203c60c928453
Nguyễn Hoàng Anh <hoahoanganh20012001@gmail.com>
2022-07-09 20:41:10 +0700

    handle unhandledRejection

--- natours/server.js
+++ natours/server.js
@@ -2,6 +2,15 @@ const app = require('./app');
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
 	console.log(`Sever running on ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+	console.log(err.name, err.message);
+
+	server.close(() => {
+		// eslint-disable-next-line no-process-exit
+		process.exit(1);
+	});
 });
