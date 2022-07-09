689448235c68c98e09bc2568e0911fb84db5d712
Nguyễn Hoàng Anh <hoahoanganh20012001@gmail.com>
2022-07-10 04:08:38 +0700

    data sanitization

--- natours/app.js
+++ natours/app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const routes = require('./router');
 const { isDev } = require('./constants');
@@ -34,6 +36,11 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
 // body parser
 app.use(express.json({ limit: '10kb' }));
 
