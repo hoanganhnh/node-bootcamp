f896294d15114a11f0c399134330d01008b329d0
Nguyễn Hoàng Anh <hoahoanganh20012001@gmail.com>
2022-07-09 20:34:57 +0700

    handle cast, duplicate, validation error

--- natours/middlewares/globalErrorHandler.js
+++ natours/middlewares/globalErrorHandler.js
@@ -1,3 +1,26 @@
+/* eslint-disable no-param-reassign */
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+	const message = `Invalid ${err.path}: ${err.value} !`;
+	return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+	const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+	console.log(value);
+
+	const message = `Duplicate field value: ${value}. Please use another value !`;
+	return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+	const errors = Object.values(err.errors).map((el) => el.message);
+
+	const message = `Invalid input data. ${errors.join('. ')} !`;
+	return new AppError(message, 400);
+};
+
 const sendErrorDev = (error, err, res) => {
 	res.status(error.statusCode).json({
 		status: error.status,
@@ -14,6 +37,9 @@ const sendErrorProduct = (error, err, res) => {
 			message: err.message,
 		});
 	} else {
+		//  Error unknown
+		console.error('ERROR 💥', err);
+
 		res.status(500).json({
 			status: 'error',
 			meassge: 'Something went very wrong !',
@@ -29,7 +55,21 @@ const globalErrorHandler = (err, req, res, next) => {
 
 	if (process.env.NODE_ENV === 'development') {
 		sendErrorDev(error, err, res);
-	} else if (process.env.NODE_ENV === 'product') {
+	} else if (
+		process.env.NODE_ENV === 'product' ||
+		process.env.NODE_ENV.includes('product')
+	) {
+		console.log(err.name);
+		if (err.name === 'CastError') {
+			err = handleCastErrorDB(err);
+		}
+		if (err.code === 11000) {
+			err = handleDuplicateFieldsDB(err);
+		}
+
+		if (err.name === 'ValidationError') {
+			err = handleValidationErrorDB(err);
+		}
 		sendErrorProduct(error, err, res);
 	}
 
