e7c150e6f8c943ec6d0ca71a05c1a59bd1d2bf85
Nguyễn Hoàng Anh <hoahoanganh20012001@gmail.com>
2022-07-10 04:13:09 +0700

    prevent parameter pollution

--- natours/app.js
+++ natours/app.js
@@ -5,6 +5,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const routes = require('./router');
 const { isDev } = require('./constants');
@@ -41,6 +42,10 @@ app.use(mongoSanitize());
 
 // Data sanitization against XSS
 app.use(xss());
+
+// Prevent parameter pollution
+app.use(hpp());
+
 // body parser
 app.use(express.json({ limit: '10kb' }));
 
