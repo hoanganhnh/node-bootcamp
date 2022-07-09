ad95531a136ed2b82d058dd794c1f8139edee50e
Nguyễn Hoàng Anh <hoahoanganh20012001@gmail.com>
2022-07-09 21:37:03 +0700

    managing password

--- natours/models/user.model.js
+++ natours/models/user.model.js
@@ -1,5 +1,7 @@
+/* eslint-disable func-names */
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
 	name: {
@@ -26,11 +28,29 @@ const userSchema = new mongoose.Schema({
 		type: String,
 		required: [true, 'User must have a passwordConfirm'],
 		minlength: 8,
+		validate: {
+			validator(ele) {
+				return ele === this.password;
+			},
+			message: 'Password is not same !',
+		},
 	},
 	role: {
 		type: String,
 		default: 'user',
 	},
+});
+
+userSchema.pre('save', async function (next) {
+	// Only run this func if password was actually modified
+	if (!this.isModified('password')) {
+		return next();
+	}
+	// Hash the password with cost of 12
+	this.password = await bcrypt.hash(this.password, 12);
+	this.passwordConfirm = undefined;
+
+	return next();
 });
 
 const UserModel = mongoose.model('user', userSchema);
