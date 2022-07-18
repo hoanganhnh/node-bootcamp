09ebff0da407fd489e1fd9bb95bd4d7999c9911c
Nguyễn Hoàng Anh <hoahoanganh20012001@gmail.com>
2022-07-18 22:26:44 +0700

    Improving Read Performance with Indexes

--- natours/models/tour.model.js
+++ natours/models/tour.model.js
@@ -126,6 +126,9 @@ const tourSchema = new mongoose.Schema(
 	},
 );
 
+tourSchema.index({ price: 1, ratingsAverage: -1 });
+tourSchema.index({ slug: 1 });
+
 // Virtual Properties
 tourSchema.virtual('durationWeeks').get(function () {
 	return this.duration / 7;
